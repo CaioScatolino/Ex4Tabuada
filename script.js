547e1ea0cdd037840bb9cf6d19b3d8c1607d3a19
Caio Scatolino <66438920+CaioScatolino@users.noreply.github.com>
2022-11-15 09:47:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function tabuada(numero) {
-    let i = 1
-    while (i <= 10) {
-        console.log(`${i} x ${numero * i}`)
-        i++
-    }
+for(let indice=1;indice<=20;indice++){
+    //bloco de cÃ³digo
+    
+    let numero = 1
+    document.getElementById("p").innerHTML += `${numero} x ${indice} = ${numero * indice}` + "<br>";
+
+    
 }
 
-tabuada(2)
